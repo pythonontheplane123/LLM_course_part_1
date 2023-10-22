3ee6d96088895121c2e9235d95175a417aacc793
gnoparus <surapong@gmail.com>
2023-10-22 12:40:02 +0700

    message should be cl.Message not str

--- langchain_integration.py
+++ langchain_integration.py
@@ -16,10 +16,10 @@ def main():
     cl.user_session.set("llm_chain",llm_chain)
 
 @cl.on_message
-async def main(message : str):
+async def main(message : cl.Message):
     llm_chain = cl.user_session.get("llm_chain")
 
-    res = await llm_chain.acall(message, callbacks=[cl.AsyncLangchainCallbackHandler()])
+    res = await llm_chain.acall(message.content, callbacks=[cl.AsyncLangchainCallbackHandler()])
 
     await cl.Message(content=res["text"]).send()
 
